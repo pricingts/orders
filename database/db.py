8c08f6f9961e66541f24050220ae14c7becde50c
Shadia Jaafar <sjaafar@tradingsolutions.com>
2025-08-04 17:59:18 -0500

    db

--- database/db.py
+++ database/db.py
@@ -5,7 +5,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
+
+# DATABASE_URL = os.getenv("DATABASE_URL")
+
+# engine = create_engine(DATABASE_URL)
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
